df07b501cbd7e15a66fde14479d7db9677ad7879
mohitbakshi00 <mohit19bakshi@gmail.com>
2023-11-03 01:08:23 +0530

    Created admin interface

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 class Category(models.Model):
     cat_id=models.AutoField(primary_key=True)
@@ -8,6 +9,12 @@ class Category(models.Model):
     image = models.ImageField(upload_to='category/')
     add_date = models.DateTimeField(auto_now_add=True, null=True)
 
+    # format image
+    def image_tag(self):
+        return format_html('<img src = "/media/{}" style ="width:40px; height:40px;" />'.format(self.image))
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model): 
     post_id = models.AutoField(primary_key=True)   
@@ -16,3 +23,6 @@ class Post(models.Model):
     url=models.CharField(max_length=100)
     cat= models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post/')
+
+    def __str__(self):
+        return self.title
